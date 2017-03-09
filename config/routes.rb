db93f15a628ef6de87f6dd89c538a2a83eb69e26
Nathan Anderson <nallenanderson@gmail.com>
2017-03-08 23:02:17 -0300

    Testing for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   get '/', to: 'pages#home'
 
   get '/about', to: 'pages#about'
@@ -8,4 +12,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get 'survey/:id', to: 'pages#survey'
+
+  match '/404', to: 'errors#not_found', via: :all
+
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
